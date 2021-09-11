8fd8eaf45325380ccb93f7138d094dc0ddd4a7ac
AdiPatel892 <84610911+AdiPatel892@users.noreply.github.com>
2021-09-11 22:06:47 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+const [value, setValue] = useState('')
+const [item, setItem] = useState([])
+const additem = ()=>{
+  if(value===""){
+
+  }
+  else{
+  setItem([...item, value ])}
+  setValue("")
+
+}
+const removeall= ()=>{
+  setItem([])
+}
+const deleteit =(ind)=>{ 
+  // console.log(ind)
+  const removedone= item.filter((elem, indexed)=>{
+return(indexed!==ind)
+console.log(indexed)
+
+  })
+  setItem(removedone)
+  
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className="main">
+       <h1 className="logo">ToDo List</h1>
+       <div className="inputs">
+         <input
+         type="text"
+         placeholder="Enter your List"
+          value={value}
+          maxLength="10"
+          onChange={(e)=>{
+            setValue(e.target.value)
+          }}
+         />
+        <button className="add" onClick={additem}
+         >Add</button>
+         
+       </div>
+       <div className="items">
+         {item.map((val, index)=>{
+           return(<div className="item" key={index}>
+           <div >{val}</div>
+           <button className="delete" onClick={()=>deleteit(index)}>Delete</button>
+           </div>
+           )  })}
+       </div>
+         <button className="remove" onClick={removeall}>Remove All</button>
+     </div>
+    </>
   );
 }
 
